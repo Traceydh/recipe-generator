68220d561bd4194ee1474a241ff60049278cb426
tracey <tracey.d.huang@gmail.com>
2022-08-29 05:07:06 +1000

    If no recipes in databse or user types return

--- script.js
+++ script.js
@@ -10,14 +10,21 @@ function getRecipes() {
 	.then(response => response.json())
 	.then(data => {
         console.log(data)
-        for (let i = 0; i < data.meals.length; i ++) {
-            console.log(data.meals[i]);
+
+        //if user typos or no recipes in database
+        if (data.meals === null) {
+            console.log('no recipes yet ')
+            return
         }
+
+        createRecipe(data.meals);
     })
 	.catch(err => console.error(err));
 }
 
 
-function addRecipe() {
-    
+function createRecipe(recipe) {
+    for (let i = 0; i < recipe.length; i ++) {
+        console.log(recipe[i]);
+    }
 }
