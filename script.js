78bf71ce2cc89152ee73d736cfbf1b08832e5dfd
tracey <tracey.d.huang@gmail.com>
2022-08-29 05:14:05 +1000

    Test connectivity of loop and create recipe function'

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Select button 
 let button = document.querySelector('#find');
-button.addEventListener('click', getRecipes)
+const recipeContainer = document.querySelector('#recipes');
+button.addEventListener('click', getRecipes);
 
 
 function getRecipes() {
@@ -10,21 +11,21 @@ function getRecipes() {
 	.then(response => response.json())
 	.then(data => {
         console.log(data)
-
         //if user typos or no recipes in database
         if (data.meals === null) {
             console.log('no recipes yet ')
             return
         }
 
-        createRecipe(data.meals);
+        createRecipeInDom(data.meals);
     })
 	.catch(err => console.error(err));
 }
 
 
-function createRecipe(recipe) {
+function createRecipeInDom(recipe) {
     for (let i = 0; i < recipe.length; i ++) {
-        console.log(recipe[i]);
+        recipeContainer.textContent += recipe[i].strMeal;
     }
 }
+
