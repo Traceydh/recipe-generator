5dde9e2e262b5cd146879ed3bbf7d0378dc0b721
tracey <tracey.d.huang@gmail.com>
2022-08-29 06:31:28 +1000

    Connect search and button

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ button.addEventListener('click', getRecipes);
 //API
 const APP_ID = '23647cb2';
 const APP_KEYS = '1ace64e82efde8a6cdfceeaee1b79d84';
-const URL = `https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=${APP_ID}&app_key=${APP_KEYS}`;
+const URL = `https://api.edamam.com/api/recipes/v2?type=public&q=`;
 
 function getRecipes() {
     let ingredient = document.querySelector('#ingredient').value;
 
-    fetch(URL)
+    fetch(`${URL}${ingredient}&app_id=${APP_ID}&app_key=${APP_KEYS}`)
 	.then(response => response.json())
 	.then(data => {
         console.log(data)
