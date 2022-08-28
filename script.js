6e45b3e7417472320a1b61daed208d305a131cb4
tracey <tracey.d.huang@gmail.com>
2022-08-29 05:02:43 +1000

    Console log loop going through each recipe returned from search to test logic

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken`)
+//Select button 
+let button = document.querySelector('#find');
+button.addEventListener('click', getRecipes)
+
+
+function getRecipes() {
+    let ingredient = document.querySelector('#ingredient').value;
+    console.log(ingredient);
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
 	.then(response => response.json())
 	.then(data => {
         console.log(data)
+        for (let i = 0; i < data.meals.length; i ++) {
+            console.log(data.meals[i]);
+        }
     })
 	.catch(err => console.error(err));
+}
+
+
+function addRecipe() {
+    
+}
